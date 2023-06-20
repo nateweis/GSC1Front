9669264a5804d341bcea07f66f7cf25892813012
nateweis <naftaliweiss21@gmail.com>
2023-06-19 22:05:33 -0400

    added inital files

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const App = () => {
   return(
-    <div>Blank Canvas</div>
+    <div>Still Blank Canvas that works </div>
   )
 }
 
--- src/index.js
+++ src/index.js
@@ -1,14 +1,22 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './index.css';
+import {Provider} from 'react-redux';
+import {configureStore, combineReducers} from '@reduxjs/toolkit';
 import App from './App';
 
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const reducer = combineReducers({
+   
+})
 
+
+const store = configureStore({reducer})
+
+ReactDOM.render(
+  <Provider store = {store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
 
