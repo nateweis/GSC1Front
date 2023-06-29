1692f3b1b4f358073f274cb8e5fae2634e22f74d
nateweis <naftaliweiss21@gmail.com>
2023-06-28 21:11:51 -0400

    a button an link placeholder to nav

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -7,9 +7,19 @@ const Nav = () => {
   return (
     <Container>
         <Left>
-            <LinkBtnWrapper><StyledIcon /> small</LinkBtnWrapper>
-            <FullLinks>BIG</FullLinks>
+            <LinkBtn><StyledIcon /></LinkBtn>
+
+            <FullSize>
+                <LinkContainer>
+                    <LinksWrapper> <Link>Home</Link> </LinksWrapper>
+                    <LinksWrapper> <Link>About</Link> </LinksWrapper>
+                    <LinksWrapper> <Link>Projects</Link> </LinksWrapper>
+                    <LinksWrapper> <Link>Blog</Link> </LinksWrapper>
+                    <LinksWrapper> <Link>Contact</Link> </LinksWrapper>
+                </LinkContainer>
+            </FullSize>
         </Left>
+
         <Right></Right>
     </Container>
   )
@@ -32,33 +42,61 @@ const Sides = styled.div`
 `
 // Left side 
 const Left = styled(Sides)`
-    //background-color: pink;
+    // background-color: pink;
     ${desc({flex: '2'})}
 `
 
-const LinkBtnWrapper = styled.div`
+const LinkBtn = styled.div`
     color: #ffffff84;
+    border: 1px solid #ffffff84;
+    border-radius: 5px;
+    margin-left: 4%;
+    cursor: pointer;
     ${desc({display: 'none'})}
 `
 const StyledIcon = styled(ILines)`
     width: 15px;
     height: 15px;
     fill: #ffffff84;
+    padding: 7px;
 `
 
-const FullLinks = styled.div`
-    color: blue;
+const FullSize = styled.div`
+    color: white;
     display: none;
-    ${desc({display: 'block'})}
+    width: 100%;
+    height: 100%;
+    ${desc({display: 'flex'})}
 `
-
+const LinkContainer = styled.div`
+    //background-color: orange;
+    width: 100%;
+    display: flex;
+    justify-content: space-around;
+    //align-items: center;
+    margin: 0 5%;
+`
+const LinksWrapper = styled.div`
+    cursor: pointer;
+    border: 1px solid white;
+    flex: 1;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    //height: 100%;
+    &:hover{
+        background-color: orange;
+        color: black;
+    }
+`
+const Link = styled.a`
+    
+`
 
 // Right side
 const Right = styled(Sides)`
     background-color: green;
 `
-
-
 
 
 
